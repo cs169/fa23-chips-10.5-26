df4e58c263b5de1735b981fba26ca4f06d78c916
Zilong Guo <zguo6@berkeley.edu>
2023-12-05 20:23:52 +0000

    Iter2 part1 app works fine

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -17,10 +17,13 @@ class Representative < ApplicationRecord
         end
       end
 
-      address = if official.address.present?
-        [official.address[0].line1, official.address[0].city, official.address[0].state, official.address[0].zip].compact.join(', ')
+      if official.address.present?
+        address = [official.address[0].line1,
+        official.address[0].city,
+        official.address[0].state,
+        official.address[0].zip].compact.join(', ')
       else
-        'Address unavailable'
+        address = 'Address unavailable'
       end
       
       party = official.party || 'Party unavailable'
@@ -32,7 +35,15 @@ class Representative < ApplicationRecord
         existing_representative.update(contact_address: address, political_party: party, photo_url: photo_url)
         reps.push(existing_representative)
       else
-        rep = Representative.create!({ name: official.name, ocdid: ocdid_temp, title: title_temp, contact_address: address, political_party: party, photo_url: photo_url})
+        rep = Representative.create!(
+          name: official.name,
+          ocdid: ocdid_temp,
+          title: title_temp,
+          contact_address: address,
+          political_party: party,
+          photo_url: photo_url
+        )
+
         reps.push(rep)
       end
     end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -6,6 +6,4 @@ RSpec.describe ApplicationRecord, type: :model do
   it 'is an abstract class' do
     expect(described_class.abstract_class).to be_truthy
   end
-
-  
 end
